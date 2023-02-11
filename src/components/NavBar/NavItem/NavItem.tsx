2f0985b691989e0f3cda9237a44dff1e0f6d9817
Julia-yes <87761523+Julia-yes@users.noreply.github.com>
2023-02-11 20:36:04 +0400

    RSC-4 feat(burger menu): fix nav bar (#32)
    * RSC-4 feat(burger menu): add template for nav bar
    
    * RSC-4 refactor(burger-menu): merge develop
    
    * RSC-4 feat(burger menu): add mui components and implement functionality for all buttons
    
    * RSC-4 refactor(burger menu): fix bug with modal window, fix bug after merge develop, delete mui/core
    
    * RSC-4 refactor(burger menu): delete unused component
    
    * RSC-4 refactor(burger menu): change functionality for drawer and modal window, fix app file, delete unused classes, move assets
    
    * RSC-4 refactor(burger menu): move to return undetectable code
    
    * RSC-4 refactor(burger menu): fix bug with Firebase API key
    
    * RSC-4 refactor(burger menu): fix bug with Firebase API key
    
    * RSC-4 refactor(burger menu): fix bug from console
    
    * RSC-4 refactor(burger menu): fix bug with translate

--- src/components/NavBar/NavItem/NavItem.tsx
+++ src/components/NavBar/NavItem/NavItem.tsx
@@ -20,14 +20,15 @@ interface NavItemProps {
 
 export const NavItem = ({ icon, name, enumData }: NavItemProps) => {
   const { t } = useTranslation();
-  const [open, setOpen] = useState(false);
   const [param, setParam] = useState(enumData[0]);
 
-  const handleOpen = () => {
-    setOpen(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
-    setOpen(false);
+    setAnchorEl(null);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,26 +36,30 @@ export const NavItem = ({ icon, name, enumData }: NavItemProps) => {
   };
   return (
     <div className={styles.navItem}>
-      <ListItem onClick={handleOpen}>
+      <ListItem
+        onClick={(event) => {
+          handleClick(event);
+        }}
+      >
         <ListItemIcon>
           <span className='material-icons'>{icon}</span>
         </ListItemIcon>
         <div>
           <ListItemText primary={t(`${name}`)} />
-          <div className={styles.value}>{t(`${param}`)}</div>
+          <div className={styles.value}>{name === 'Mode' ? t(`${param}`) : param}</div>
         </div>
       </ListItem>
       <Menu
         id='simple-menu'
         keepMounted
         open={open}
+        anchorEl={anchorEl}
         onClick={handleClose}
         anchorOrigin={{
           vertical: 'top',
           horizontal: 'left',
         }}
         sx={{
-          top: '200px',
           left: '50px',
         }}
       >
@@ -87,7 +92,7 @@ export const NavItem = ({ icon, name, enumData }: NavItemProps) => {
                 <FormControlLabel
                   value={item}
                   control={<Radio color={'primary'} />}
-                  label={t(`${item}`)}
+                  label={name === 'Mode' ? t(`${item}`) : item}
                   key={item}
                   sx={{
                     padding: '10px',
