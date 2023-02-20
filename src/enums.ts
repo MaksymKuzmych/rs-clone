fd5c32dd497ae5e0b2340d8e2f4c9270a5266050
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-02-20 18:38:53 +0200

    RSC-15 fix(auth): fix provider auth

--- src/enums.ts
+++ src/enums.ts
@@ -39,3 +39,8 @@ export enum Sort {
   ASC = 'asc',
   DESC = 'desc',
 }
+
+export enum Provider {
+  Google = 'Google',
+  Github = 'Github',
+}
---
+++ src/firebase/sign-in-provider.ts
@@ -0,0 +1,40 @@
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  linkWithPopup,
+  signInWithPopup,
+} from 'firebase/auth';
+import { Provider } from '../enums';
+import { auth, FirebaseError } from './firebase-config';
+
+export const signInProvider = async (provider: Provider) => {
+  const CREDENTIALS_ERROR = 'FirebaseError: Firebase: Error (auth/credential-already-in-use).';
+  const EMAIL_ERROR = 'FirebaseError: Firebase: Error (auth/email-already-in-use).';
+  let authProvider = new GoogleAuthProvider();
+  let message = Provider.Google;
+  if (provider === Provider.Github) {
+    authProvider = new GithubAuthProvider();
+    message = Provider.Github;
+  }
+  try {
+    try {
+      if (auth.currentUser) {
+        await linkWithPopup(auth.currentUser, authProvider);
+      } else {
+        await signInWithPopup(auth, authProvider);
+      }
+    } catch (error) {
+      switch (`${error}`) {
+        case CREDENTIALS_ERROR:
+          await signInWithPopup(auth, authProvider);
+          break;
+        case EMAIL_ERROR:
+          throw new FirebaseError('Email Already Exist');
+        default:
+          throw new FirebaseError(`${error}`);
+      }
+    }
+  } catch (error) {
+    throw new FirebaseError(`Sign In ${message}: ${error}`);
+  }
+};
